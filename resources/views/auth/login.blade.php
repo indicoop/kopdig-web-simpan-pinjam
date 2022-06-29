ef1d9941b6a3bea91f18db298e36ea72b0634b4b
E41200415NOGA <nogamuktiwati@gmail.com>
2022-06-30 05:24:05 +0700

    mengupdate copywriting sinjam

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,7 +62,7 @@
     <head>
 
         <meta charset="utf-8" />
-        <title>Login| Simpan Pinjam</title>
+        <title>Login | Simpan Pinjam</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
         <meta content="Themesbrand" name="author" />
@@ -111,13 +111,13 @@
                                                         </div>
                                                         <div class="carousel-inner text-center text-white-50 pb-5">
                                                             <div class="carousel-item active">
-                                                                <p class="fs-15 fst-italic">" Great! Clean code, clean design, easy for customization. Thanks very much! "</p>
+                                                                <p class="fs-15 fst-italic">" Mudah dan Transaksi Lebih Terlihat! "</p>
                                                             </div>
                                                             <div class="carousel-item">
-                                                                <p class="fs-15 fst-italic">" The theme is really great with an amazing customer support."</p>
+                                                                <p class="fs-15 fst-italic">"Mudah mengetahui angsuran simpan pinjam."</p>
                                                             </div>
                                                             <div class="carousel-item">
-                                                                <p class="fs-15 fst-italic">" Great! Clean code, clean design, easy for customization. Thanks very much! "</p>
+                                                                <p class="fs-15 fst-italic">"Lebih banyak yang ikut koperasi "</p>
                                                             </div>
                                                         </div>
                                                     </div>
@@ -131,8 +131,8 @@
                                     <div class="col-lg-6">
                                         <div class="p-lg-5 p-4">
                                             <div>
-                                                <h5 class="text-primary">Welcome Back !</h5>
-                                                <p class="text-muted">Sign in to continue KOPDIG Simpan Pinjam.</p>
+                                                <h5 class="text-primary">Selamat Datang Koperasi Simpan Pinjam</h5>
+                                                <p class="text-muted">Silakan masuk KOPDIG Simpan Pinjam terlebih dahulu.</p>
                                             </div>
 
                                             <div class="mt-4">
@@ -141,7 +141,7 @@
 
                                                     <div class="mb-3">
                                                         <label for="email" class="form-label">Email</label>
-                                                        <input type="text"name="email" required autofocus class="form-control" id="email" placeholder="Enter email">
+                                                        <input type="text"name="email" required autofocus class="form-control" id="email" placeholder="Masukkan Email Anda">
                                                     </div>
 
                                                     <div class="mb-3">
@@ -150,18 +150,18 @@
                                                         </div> --}}
                                                         <label class="form-label" for="password-input">Password</label>
                                                         <div class="position-relative auth-pass-inputgroup mb-3">
-                                                            <input type="password" name="password" required autofocus="current-password" class="form-control pe-5" placeholder="Enter password" id="password-input">
+                                                            <input type="password" name="password" required autofocus="current-password" class="form-control pe-5" placeholder="Masukkan Password Anda" id="password-input">
                                                             <button class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted" type="button" id="password-addon"><i class="ri-eye-fill align-middle"></i></button>
                                                         </div>
                                                     </div>
 
                                                     <div class="form-check">
                                                         <input class="form-check-input" type="checkbox" value="" id="auth-remember-check" name="remember">
-                                                        <label class="form-check-label" for="auth-remember-check">Remember me</label>
+                                                        <label class="form-check-label" for="auth-remember-check">Ingatkan Saya</label>
                                                     </div>
 
                                                     <div class="mt-4">
-                                                        <button class="btn btn-success w-100" type="submit">Sign In</button>
+                                                        <button class="btn btn-success w-100" type="submit">Login</button>
                                                     </div>
 
                                                     {{-- <div class="mt-4 text-center">
